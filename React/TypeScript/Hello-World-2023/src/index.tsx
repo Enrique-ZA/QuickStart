2f3a9a5125282dc8e11418a26272b30ba3869109
Enrique-ZA <60000947+Enrique-ZA@users.noreply.github.com>
2023-07-09 14:30:23 +0200

    uploaded files

---
+++ React/TypeScript/Hello-World-2023/src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App: React.FC = () => (
+  <div>
+    <h1>Hello, world!</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
---
+++ React/TypeScript/Hello-World-2023/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
+
